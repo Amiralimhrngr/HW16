7f37110432e825c0255c57f7a362c24ede555cff
Amiralimhrngr <ftmr92@gmail.com>
2026-07-06 01:48:27 +0330

    feat(MemberRepository): add addMember

--- src/repository/MemberRepository.java
+++ src/repository/MemberRepository.java
@@ -1,5 +1,24 @@
 package repository;
 
-public class MemberRepository {
+import exception.DatabaseOperationException;
+import model.Member;
+import util.DatabaseConfig;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class MemberRepository {
+    public int addMember(Member member) {
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO member(full_name, phone) VALUES (?,?)")
+        ) {
+            preparedStatement.setString(1, member.getFullName());
+            preparedStatement.setString(2, member.getPhone());
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DatabaseOperationException("Member insertion failed: " + e.getMessage());
+        }
+    }
 }
