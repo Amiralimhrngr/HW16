fbd398ce629a73fd29bd3de2c85261eec64115dd
Amiralimhrngr <ftmr92@gmail.com>
2026-07-06 01:57:34 +0330

    feat(MemberRepository): add findMember

--- src/repository/MemberRepository.java
+++ src/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package repository;
 
 import exception.DatabaseOperationException;
+import exception.MemberNotFoundException;
 import model.Member;
 import util.DatabaseConfig;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MemberRepository {
@@ -19,6 +21,23 @@ public class MemberRepository {
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new DatabaseOperationException("Member insertion failed: " + e.getMessage());
+        }
+    }
+
+    public Member findMember(long id) {
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM member WHERE id = ?")
+        ) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                return new Member(resultSet.getLong("id"), resultSet.getString("full_name"), resultSet.getString("phone"));
+            } else {
+                throw new MemberNotFoundException("Couldn't find member with id: " + id);
+            }
+        } catch (SQLException e){
+            throw new DatabaseOperationException("Member find operation failed: " + e.getMessage());
         }
     }
 }
