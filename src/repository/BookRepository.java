1d3f807ed41aa46befee8a386553e947e5f1860e
Amiralimhrngr <ftmr92@gmail.com>
2026-07-06 01:06:50 +0330

    feat(BookRepository): add find book method

--- src/repository/BookRepository.java
+++ src/repository/BookRepository.java
@@ -1,11 +1,13 @@
 package repository;
 
-import exception.BookInsertionException;
+import exception.DatabaseOperationException;
+import exception.BookNotFoundException;
 import model.Book;
 import util.DatabaseConfig;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BookRepository {
@@ -21,8 +23,26 @@ public class BookRepository {
             preparedStatement.setDouble(3, book.getPrice());
             preparedStatement.setInt(4, book.getStock());
             return preparedStatement.executeUpdate();
-        } catch (SQLException e){
-            throw new BookInsertionException("Book insertion failed: " + e.getMessage());
+        } catch (SQLException e) {
+            throw new DatabaseOperationException("Book insertion failed: " + e.getMessage());
         }
     }
+
+    public Book findBook(long id) {
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM book WHERE id = ?")
+        ) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Book(id, resultSet.getString("title"), resultSet.getString("author"), resultSet.getDouble("price"), resultSet.getInt("stock"));
+            } else {
+                throw new BookNotFoundException("Couldn't find book with id: " + id);
+            }
+        } catch (SQLException e) {
+            throw new DatabaseOperationException("Book find operation failed: " + e.getMessage());
+        }
+    }
+
 }
