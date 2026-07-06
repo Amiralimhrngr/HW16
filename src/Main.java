742ae04888df825f891f6aa5c16691551fedd0ed
Amiralimhrngr <ftmr92@gmail.com>
2026-07-07 00:27:45 +0330

    feat(main): add data examples to database

--- src/Main.java
+++ src/Main.java
@@ -19,10 +19,18 @@ public class Main {
         Book book3 = new Book("Head First Java", "Kathy Sierra", 39.99, 12);
         Book book4 = new Book("Java: The Complete Reference", "Herbert Schildt", 60.00, 5);
         Book book5 = new Book("Design Patterns", "Erich Gamma", 49.75, 8);
+        bookRepository.addBook(book1);
+        bookRepository.addBook(book2);
+        bookRepository.addBook(book3);
+        bookRepository.addBook(book4);
+        bookRepository.addBook(book5);
 
         Member member1 = new Member("John Doe", "09121234567");
         Member member2 = new Member("Jane Doe", "09129876543");
         Member member3 = new Member("Mike Tyson", "09351234567");
+        memberRepository.addMember(member1);
+        memberRepository.addMember(member2);
+        memberRepository.addMember(member3);
 
         menu:
         while (true) {
