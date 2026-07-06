e7f0d7638aa7ad1180476d3968a28a547a1359fb
Amiralimhrngr <ftmr92@gmail.com>
2026-07-07 00:59:22 +0330

    feat(main): add showAllMembers and showAllBooks

--- src/Main.java
+++ src/Main.java
@@ -19,18 +19,30 @@ public class Main {
         Book book3 = new Book("Head First Java", "Kathy Sierra", 39.99, 12);
         Book book4 = new Book("Java: The Complete Reference", "Herbert Schildt", 60.00, 5);
         Book book5 = new Book("Design Patterns", "Erich Gamma", 49.75, 8);
-        bookRepository.addBook(book1);
-        bookRepository.addBook(book2);
-        bookRepository.addBook(book3);
-        bookRepository.addBook(book4);
-        bookRepository.addBook(book5);
 
         Member member1 = new Member("John Doe", "09121234567");
         Member member2 = new Member("Jane Doe", "09129876543");
         Member member3 = new Member("Mike Tyson", "09351234567");
-        memberRepository.addMember(member1);
-        memberRepository.addMember(member2);
-        memberRepository.addMember(member3);
+        try {
+            bookRepository.addBook(book1);
+            bookRepository.addBook(book2);
+            bookRepository.addBook(book3);
+            bookRepository.addBook(book4);
+            bookRepository.addBook(book5);
+            memberRepository.addMember(member1);
+            memberRepository.addMember(member2);
+            memberRepository.addMember(member3);
+
+            Book updatedBook = bookRepository.updateBook(1, 50.00);
+            System.out.println(updatedBook);
+
+            bookRepository.deleteBook(2);
+            memberRepository.deleteMember(1);
+        } catch (BookNotFoundException | MemberNotFoundException | DatabaseOperationException e) {
+            System.out.println(e.getMessage());
+        }
+
+
 
         menu:
         while (true) {
@@ -41,7 +53,9 @@ public class Main {
                     3. Update the Price of a Book
                     4. Delete a Book
                     5. Delete a Member
-                    6. Exit
+                    6. Show All Books
+                    7. Show All Members
+                    8. Exit
                     """);
             int choice = scanner.nextInt();
             scanner.nextLine();
@@ -94,7 +108,7 @@ public class Main {
                             System.out.printf("Book updated successfully! Book with ID: %d price set to $%.2f",
                                     updatedBook.getId(), updatedBook.getPrice());
                         }
-                    } catch (BookNotFoundException e) {
+                    } catch (BookNotFoundException | DatabaseOperationException e) {
                         System.out.println(e.getMessage());
                     }
                     break;
@@ -107,7 +121,7 @@ public class Main {
                         if (booksDeleted > 0) {
                             System.out.println("Book deleted successfully!");
                         }
-                    } catch (BookNotFoundException e) {
+                    } catch (BookNotFoundException | DatabaseOperationException e) {
                         System.out.println(e.getMessage());
                     }
                     break;
@@ -120,11 +134,17 @@ public class Main {
                         if (membersDeleted > 0) {
                             System.out.println("Member Deleted Successfully!");
                         }
-                    } catch (MemberNotFoundException e) {
+                    } catch (MemberNotFoundException | DatabaseOperationException e) {
                         System.out.println(e.getMessage());
                     }
                     break;
                 case 6:
+                    bookRepository.showAllBooks();
+                    break;
+                case 7:
+                    memberRepository.showAllMembers();
+                    break;
+                case 8:
                     scanner.close();
                     System.out.println("Exiting...");
                     break menu;
